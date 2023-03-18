6384a62cb0a5610c31105eff8df2a8d22ca82fab
Jae Han Song <rfv1479@gmail.com>
2023-03-19 07:03:43 +0900

    hotfix : 지도 디자인 다시 수정

--- components/MyReservationDisplayCard/index.jsx
+++ components/MyReservationDisplayCard/index.jsx
@@ -18,7 +18,7 @@ export default function MyReservationDisplayCard({ reservationDataList }) {
 
       {/* 예약한 것이 없을때 */}
       {reservationDataList.length == 0 ? (
-        <div className="rounded my-3 shadow p-4">
+        <div className="bg-white rounded my-3 shadow p-4">
           <p className="my-10 text-center text-sm text-neutral-700">예약된 공간이 없습니다</p>
         </div>
       ) : (
