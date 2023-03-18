45072784bd8ea35ded9d65ccb3cc5a553f63d57e
seolcu <seolcu0112@proton.me>
2023-03-19 01:38:37 +0900

    history display card component

--- app/notice/page.jsx
+++ app/notice/page.jsx
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/react';
 import Link from 'node_modules/next/link';
 
 export default function Notice() {
-  // 예시 API 데이터
+  // 예시 예약현황 데이터
   const exampleReservationDataList = [
     {
       place: '대운동장',
@@ -19,6 +19,24 @@ export default function Notice() {
       date: '2023-03-18',
     },
   ];
+
+  const exampleReservationHistoryList = [
+    {
+      place: '대운동장',
+      time: 15,
+      date: '2023-03-17',
+    },
+    {
+      place: '소운동장',
+      time: 18,
+      date: '2023-03-16',
+    },
+    {
+      place: '헬스장',
+      time: 9,
+      date: '2023-03-14',
+    },
+  ];
   return (
     <main className="pt-4 bg-[#fafafa]">
       <Link href="/" className="text-2xl font-bold">
@@ -29,7 +47,7 @@ export default function Notice() {
         <MyReservationDisplayCard reservationDataList={exampleReservationDataList} />
         <hr className="rounded-full my-5 border-2 border-primary-main w-10 text-center mx-auto"></hr>
         <h2 className="font-bold text-lg my-5">사용이력</h2>
-        <MyHistoryDisplayCard />
+        <MyHistoryDisplayCard reservationHistoryList={exampleReservationHistoryList} />
       </div>
     </main>
   );
