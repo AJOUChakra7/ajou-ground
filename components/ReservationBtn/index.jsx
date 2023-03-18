f345f8cdc69822d4c42d955f16bf7ba0fe5a7715
seolcu <seolcu0112@proton.me>
2023-03-18 21:39:29 +0900

    reservation component

---
+++ components/ReservationBtn/index.jsx
@@ -0,0 +1,23 @@
+import Link from 'node_modules/next/link';
+
+export default function ReservationBtn({ time, status, selected, value, setSelectedTime }) {
+  return (
+    <Link href="/reservation">
+      <div
+        onClick={() => setSelectedTime(value)}
+        className={`rounded-lg p-3 ${
+          status == true && selected == true
+            ? 'bg-blue-600 text-white'
+            : status == true && selected == false
+            ? 'bg-sky-50 border-2 border-blue-500'
+            : 'bg-neutral-200'
+        }`}
+      >
+        <h2 className="text-xs text-left">{time}</h2>
+        <h2 className={`text-xs text-left font-bold ${status == true ? '' : 'text-neutral-500'}`}>
+          {status == true ? '예약 가능' : '예약 불가'}
+        </h2>
+      </div>
+    </Link>
+  );
+}
