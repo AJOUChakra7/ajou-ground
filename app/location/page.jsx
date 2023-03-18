e66d1f3fc6f02d03fb7a817809180b59e892be44
seolcu <seolcu0112@proton.me>
2023-03-19 06:59:59 +0900

    안돼서 롤백

--- app/location/page.jsx
+++ app/location/page.jsx
@@ -2,20 +2,17 @@
 import KakaoMap from '@components/KakaoMap/index';
 import Footer from '@components/Footer/index';
 import Script from 'next/script';
-import Head from 'node_modules/next/head';
 
 export default function Location() {
   return (
     <>
-      <Head>
-        <Script
-          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=f7da64a90767677a5c9a7b39da61bab2&libraries=services,clusterer&autoload=false`}
-          // strategy="beforeInteractive"
-          onLoad={() => {
-            console.log('Script has loaded');
-          }}
-        />
-      </Head>
+      <Script
+        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=f7da64a90767677a5c9a7b39da61bab2&libraries=services,clusterer&autoload=false`}
+        // strategy="beforeInteractive"
+        onLoad={() => {
+          console.log('Script has loaded');
+        }}
+      />
       <main>
         <h2>지도 페이지</h2>
         <KakaoMap />
