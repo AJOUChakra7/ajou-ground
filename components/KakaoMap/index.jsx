0648012db98b80254e550b8b3c4e8fab80407d52
seolcu <seolcu0112@proton.me>
2023-03-19 04:48:44 +0900

    카카오맵 - 된다고 가정하고 커밋

--- components/KakaoMap/index.jsx
+++ components/KakaoMap/index.jsx
@@ -1,9 +1,11 @@
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 export default function KakaoMap() {
-  <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: '100%', height: '360px' }}>
-    <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-      <div style={{ color: '#000' }}>Hello World!</div>
-    </MapMarker>
-  </Map>;
+  return (
+    <Map center={{ lat: 37.2830557, lng: 127.0448373 }} style={{ width: '100%', height: '360px' }}>
+      <MapMarker position={{ lat: 37.2830557, lng: 127.0448373 }}>
+        <div style={{ color: '#000' }}>Hello World!</div>
+      </MapMarker>
+    </Map>
+  );
 }
