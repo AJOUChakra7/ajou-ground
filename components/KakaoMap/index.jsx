b7c4bbf83bf7ef8a45d9d72f1fd692a06b588b12
seolcu <seolcu0112@proton.me>
2023-03-19 05:09:09 +0900

    마커 완성

--- components/KakaoMap/index.jsx
+++ components/KakaoMap/index.jsx
@@ -2,9 +2,18 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 export default function KakaoMap() {
   return (
-    <Map center={{ lat: 37.2830557, lng: 127.0448373 }} style={{ width: '100%', height: '360px' }}>
-      <MapMarker position={{ lat: 37.2830557, lng: 127.0448373 }}>
-        <div style={{ color: '#000' }}>Hello World!</div>
+    <Map center={{ lat: 37.280892, lng: 127.045464 }} style={{ width: '100%', height: '360px' }}>
+      <MapMarker position={{ lat: 37.280791, lng: 127.044454 }}>
+        <div style={{ color: '#000' }}>대운동장</div>
+      </MapMarker>
+      <MapMarker position={{ lat: 37.280167, lng: 127.0444138 }}>
+        <div style={{ color: '#000' }}>소운동장</div>
+      </MapMarker>
+      <MapMarker position={{ lat: 37.280074, lng: 127.045393 }}>
+        <div style={{ color: '#000' }}>헬스장</div>
+      </MapMarker>
+      <MapMarker position={{ lat: 37.282136, lng: 127.049261 }}>
+        <div style={{ color: '#000' }}>테니스장</div>
       </MapMarker>
     </Map>
   );
