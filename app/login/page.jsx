f038664b871242390c6461ec0c18d198a703df35
seolcu <seolcu0112@proton.me>
2023-03-18 20:07:33 +0900

    login button click is redirecting to main page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,3 +1,5 @@
+import Link from 'node_modules/next/link';
+
 export const metadata = {
   title: '아주그라운드: 로그인',
 };
@@ -8,7 +10,11 @@ export default function Login() {
       <div className="flex flex-col justify-between h-screen text-center">
         <div>{/* 비율유지용 div */}</div>
         <h1 className="text-4xl font-extrabold">아주그라운드</h1>
-        <div className="m-5 p-5 bg-neutral-200 rounded font-bold text-sm">구글 계정으로 로그인</div>
+        <Link href="/">
+          <button className="m-5 p-5 bg-neutral-200 rounded font-bold text-sm">
+            구글 계정으로 로그인
+          </button>
+        </Link>
         <footer className="m-3">Team chakra7</footer>
       </div>
     </main>
