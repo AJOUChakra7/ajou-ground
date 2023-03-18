990e46af6185d5f877221f5b78bcd7f2da305be4
seolcu <seolcu0112@proton.me>
2023-03-19 06:58:12 +0900

    지도 에러: script를 next/head 안에 넣어보기

--- app/location/page.jsx
+++ app/location/page.jsx
@@ -2,22 +2,22 @@
 import KakaoMap from '@components/KakaoMap/index';
 import Footer from '@components/Footer/index';
 import Script from 'next/script';
-import Header from '@components/Header/index';
+import Head from 'node_modules/next/head';
 
 export default function Location() {
   return (
     <>
-      
-      <Script
-        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=f7da64a90767677a5c9a7b39da61bab2&libraries=services,clusterer&autoload=false`}
-        // strategy="beforeInteractive"
-        onLoad={() => {
-          console.log('Script has loaded');
-        }}
-      />
-      <main className="pt-16">
-        <Header />
-        <h2 className="font-bold text-[20px] pb-2">운동 시설 위치</h2>
+      <Head>
+        <Script
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=f7da64a90767677a5c9a7b39da61bab2&libraries=services,clusterer&autoload=false`}
+          // strategy="beforeInteractive"
+          onLoad={() => {
+            console.log('Script has loaded');
+          }}
+        />
+      </Head>
+      <main>
+        <h2>지도 페이지</h2>
         <KakaoMap />
       </main>
       <Footer />
